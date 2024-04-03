f98508d07aec900b9aadb91e7f9c64462f085fe6
Martin Garcia Hurtado <70974099+Martin-GarciaHurtado@users.noreply.github.com>
2024-04-02 22:11:21 -0600

    login and singup
    Se crea vusta para poder iniciar sesion y poder registrar a un nuevo usuario.

--- tienda/src/App.vue
+++ tienda/src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <v-app>
+    <v-app-bar :elevation="2">
+        <template v-slot:prepend>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+        <v-spacer></v-spacer>
+        <v-app-bar-title>Juan Store</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <v-btn to="/login" text>Iniciar Sesión</v-btn>
+        <v-btn to="/singup" text>Registrar</v-btn>
+    </v-app-bar>
     <v-main>
       <router-view/>
     </v-main>
@@ -12,7 +22,10 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+
   }),
+  methods: {
+
+  }
 }
 </script>
--- tienda/src/router/index.js
+++ tienda/src/router/index.js
@@ -14,6 +14,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'loginPage',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login/LoginPage.vue')
+  },
+  {
+    path: '/singup',
+    name: 'singup',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login/SingUp.vue')
   }
 ]
 
