c36ddcf76eb212c4c3694636087be99eaa707dbe
Martin Garcia Hurtado <70974099+Martin-GarciaHurtado@users.noreply.github.com>
2024-04-04 21:41:03 -0600

    Add Bottons and Dialog
    Se modifica botones de login y singup ademas de agregar boton para una nueva venta junto con el dialogo, aun falta modificar el dialogo cn los valores que ocupamos

--- tienda/src/App.vue
+++ tienda/src/App.vue
@@ -2,14 +2,37 @@
   <v-app>
     <v-app-bar :elevation="2">
         <template v-slot:prepend>
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
         </template>
         <v-spacer></v-spacer>
-        <v-app-bar-title>Juan Store</v-app-bar-title>
-        <v-spacer></v-spacer>
-        <v-btn to="/login" text>Iniciar Sesión</v-btn>
-        <v-btn to="/singup" text>Registrar</v-btn>
+        <v-row align="end" justify="end">
+          <v-col cols="auto">
+            <v-btn to="/login" text="Iniciar Sesión" variant="tonal" color="purple-lighten-3"></v-btn>
+          </v-col>
+          <v-col cols="auto">
+            <v-btn to="/singup" text="Registrar" variant="tonal" color="purple-lighten-3"></v-btn>
+          </v-col>
+          <br>
+        </v-row>
     </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+    >
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg"
+        title="Martin Garcia Hurtado"
+      ></v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-home" title="Inicio" value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Inventario" value="admin"></v-list-item>
+        <v-list-item prepend-icon="mdi-cash-register" title="Ganancias" value="cash"></v-list-item>
+        <v-list-item prepend-icon="mdi-logout" title="Cerrar Sesion" value="logout"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <router-view/>
     </v-main>
@@ -21,9 +44,11 @@
 export default {
   name: 'App',
 
-  data: () => ({
-
-  }),
+  data () {
+    return {
+      drawer: null,
+    }
+  },
   methods: {
 
   }
--- tienda/src/views/HomeView.vue
+++ tienda/src/views/HomeView.vue
@@ -1,18 +1,203 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-card flat>
+      <v-card-title class="d-flex align-center pe-2">
+        <v-icon icon="mdi-cash-register"></v-icon> &nbsp;
+        Ventas del Día
+
+        <v-spacer></v-spacer>
+
+        <v-text-field
+          v-model="search"
+          density="compact"
+          label="Search"
+          prepend-inner-icon="mdi-magnify"
+          variant="solo-filled"
+          flat
+          hide-details
+          single-line
+        ></v-text-field>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          prepend-icon="mdi-cart-plus"
+          text="Nueva Venta"
+          @click="dialog = true"
+          color="purple-darken-3"
+        ></v-btn>
+      </v-card-title>
+
+      <v-divider></v-divider>
+    </v-card>
+    <v-data-table :headers="headers" v-model:search="search" :items="items" item-value="ref">
+      
+    </v-data-table>
+
+    <div class="pa-4 text-center">
+      <v-dialog
+        v-model="dialog"
+        max-width="600"
+        persistent=""
+      >
+        <v-card
+          prepend-icon="mdi-account"
+          title="User Profile"
+        >
+          <v-card-text>
+            <v-row dense>
+              <v-col
+                cols="12"
+                md="4"
+                sm="6"
+              >
+                <v-text-field
+                  label="First name*"
+                  required
+                ></v-text-field>
+              </v-col>
+
+              <v-col
+                cols="12"
+                md="4"
+                sm="6"
+              >
+                <v-text-field
+                  hint="example of helper text only on focus"
+                  label="Middle name"
+                ></v-text-field>
+              </v-col>
+
+              <v-col
+                cols="12"
+                md="4"
+                sm="6"
+              >
+                <v-text-field
+                  hint="example of persistent helper text"
+                  label="Last name*"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col>
+
+              <v-col
+                cols="12"
+                md="4"
+                sm="6"
+              >
+                <v-text-field
+                  label="Email*"
+                  required
+                ></v-text-field>
+              </v-col>
+
+              <v-col
+                cols="12"
+                md="4"
+                sm="6"
+              >
+                <v-text-field
+                  label="Password*"
+                  type="password"
+                  required
+                ></v-text-field>
+              </v-col>
+
+              <v-col
+                cols="12"
+                md="4"
+                sm="6"
+              >
+                <v-text-field
+                  label="Confirm Password*"
+                  type="password"
+                  required
+                ></v-text-field>
+              </v-col>
+
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-select
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                  label="Age*"
+                  required
+                ></v-select>
+              </v-col>
+
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-autocomplete
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  label="Interests"
+                  auto-select-first
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+
+            <small class="text-caption text-medium-emphasis">*indicates required field</small>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn
+            color="red"
+              text="Close"
+              variant="plain"
+              @click="dialog = false"
+            ></v-btn>
+
+            <v-btn
+              color="green"
+              text="Save"
+              variant="tonal"
+              @click="dialog = false"
+            ></v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+  </v-container>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
 
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
+export default {
   name: 'HomeView',
-
-  components: {
-    HelloWorld,
+  data () {
+    return {
+      search: '',
+      dialog: false,
+      headers: [
+        { title: 'N° Referencia', align: 'start', key: 'ref' },
+        { title: 'Nombre',  key: 'name' },
+        { title: 'Tamaño', key: 'size' },
+        { title: 'Precio', key: 'price' },
+        { title: 'Cantidad', key: 'quantity' },
+        { title: 'Total', key: 'tot' }
+      ],
+      items: [
+        { ref: '000111', name: 'Playera', size: 'CH', price: 250, quantity: 10, tot: 2500 },
+        { ref: '000222', name: 'Pantalon', size: '32', price: 359, quantity: 8, tot: 2872 },
+        { ref: '000333', name: 'Camisa', size: 'M', price: 299, quantity: 15, tot: 4485 },
+        { ref: '000444', name: 'Boxer', size: 'G', price: 50, quantity: 5, tot: 250 },
+        { ref: '000555', name: 'Calcetin', size: 'Unitalla', price: 7, quantity: 10, tot: 70 }
+      ],
+    }
   },
-});
+  methods: {
+
+  },
+  components: {
+    
+  },
+};
 </script>
